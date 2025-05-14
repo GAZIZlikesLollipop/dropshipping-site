a6a92561de5cdfe7c4dcd4ce3ad5c45d770ea10e
Gaziz <gazizvalitov230378@gmail.com>
2025-05-14 19:38:44 +0500

    data base features

--- src/app/components/ProductCard.js
+++ src/app/components/ProductCard.js
@@ -1,12 +1,21 @@
 import styles from "@/app/page.module.css";
 
 export class Product {
-    constructor(id, name, price, imageUrl, description) {
+    constructor(id, name, price, imageUrl) {
         this.id = id;
         this.name = name;
         this.price = price;
         this.imageUrl = imageUrl;
-        this.description = description;
+    }
+}
+
+export class User {
+    constructor(id, name, place, payment, cart) {
+        this.id = id;
+        this.name = name;
+        this.place = place;
+        this.payment = payment;
+        this.cart = cart;
     }
 }
 
--- src/app/page.js
+++ src/app/page.js
@@ -6,9 +6,12 @@ import Link from "next/link";
 
 export default function Home() {
     const products = [
-        new Product(1, "iPhone 14", "2 431 462", "/product1.jpg", "Latest iPhone with A16 Bionic."),
-        new Product(2, "Samsung Galaxy S23", "6 543 342", "/led.jpg", "Flagship Samsung phone."),
-        new Product(3, "Google Pixel 8", "30 343", "/tool.jpg", "Smartphone with stock Android."),
+        new Product(1, "iPhone 14", "2 431 462", "/product1.jpg"),
+        new Product(2, "Samsung Galaxy S23", "6 543 342", "/led.jpg"),
+        new Product(3, "Google Pixel 8", "30 343", "/tool.jpg"),
+        new Product(4, "Minecraft board", "324 500", "/product1.jpg"),
+        new Product(5, "Кунжут чистый", "117 244", "/led.jpg"),
+        new Product(6, "ChinaPhone 16 pro max", "$ 117 321 462", "/chinaPhone.jpg"),
     ];
 
   return (
